2c66c43ee172e6c063eddc24ce491aa547b8adc9
jorgearacaty <jorgearacaty@gmail.com>
2014-08-05 20:30:39 -0300

    19:18 08/05/2014

---
+++ @.bat
@@ -0,0 +1,13 @@
+REM refresh GitHub - continua atualizando.
+REM jorgearacaty, 5 aug 2014 - 2025.
+
+git add @.bat
+git add @inicio.bat
+git add  @leiaME.docx
+git add MEleia.txt 
+git add  calcula_estatistica_de_um_intervalo.xlsm
+git add  mosaico.xlsm
+
+
+git commit -m "19:18 08/05/2014" 
+git push -u origin master 
