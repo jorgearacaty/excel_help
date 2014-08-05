e8d246a485a7551ca4d394f539b5d28fa72d4438
jorgearacaty <jorgearacaty@gmail.com>
2014-08-05 20:49:07 -0300

    19:18 08/05/2014

--- @.bat
+++ @.bat
@@ -1,12 +1,13 @@
-REM refresh GitHub - continua atualizando.
-REM jorgearacaty, 5 aug 2014 - 2025.
+@echo off
+echo refresh GitHub a cada ciclo de atualização.
+echo jorgearacaty, 5 aug 2014 - 2025.
 
 git add @.bat
-git add @start.bat
-git add  @leiaME.docx
+git add @set_remote.bat
+git add leiaME.docx
 git add MEleia.txt 
-git add  calcula_estatistica_de_um_intervalo.xlsm
-git add  mosaico.xlsm
+git add calcula_estatistica_de_um_intervalo.xlsm
+git add mosaico.xlsm
 
 
 git commit -m "19:18 08/05/2014" 
---
+++ @set_remote.bat
@@ -0,0 +1,7 @@
+REM refresh GitHub - usa apenas no processo de implantação do repo.
+REM jorgearacaty, 5 aug 2014 - 2025.
+
+REM 'cd C:\\\Users\\CCE\\Desktop\\GetGit\\excel_help'
+
+git remote add origin https://github.com/jorgearacaty/excel_help.git
+
