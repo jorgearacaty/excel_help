ec549f2458dcc77f60bbf60270fc20ce3888f5e1
Jorge Aracaty Rocha de Lima <jorgearacaty@gmail.com>
2014-08-07 11:24:47 -0300

    Delete @set_remote.bat

--- @set_remote.bat
+++
@@ -1,8 +0,0 @@
-echo --------------------------------------------------
-echo seta sincronização do repo local com o remote.
-echo jorgearacaty, 5 aug 2014 - 2051.
-echo --------------------------------------------------
-
-git remote add origin https://github.com/jorgearacaty/excel_help.git
-
-
